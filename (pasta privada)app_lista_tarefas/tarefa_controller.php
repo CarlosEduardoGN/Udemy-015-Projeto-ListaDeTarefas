c92b30d068814ba7291d348b296e8194b51302f5
Carlos Eduardo Guedes do Nascimento <nassaofsky@gmail.com>
2023-08-14 02:11:31 -0300

    first commit

---
+++ (pasta privada)app_lista_tarefas/tarefa_controller.php
@@ -0,0 +1,66 @@
+<?php
+    require "../../app_lista_tarefas/tarefa.model.php";
+    require "../../app_lista_tarefas/tarefa.service.php";
+    require "../../app_lista_tarefas/conexao.php";
+
+    $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
+
+
+    if( $acao == 'inserir'){
+        $tarefa = new Tarefa();
+        $tarefa->__set('tarefa', $_POST['tarefa']);
+    
+        $conexao = new Conexao();
+    
+        $tarefaService = new TarefaService($conexao,$tarefa);
+        $tarefaService->inserir();
+        
+        header('Location: nova_tarefa.php?inclusao=1');
+    }else if ($acao == 'recuperar'){
+        $tarefa = new Tarefa();
+        $conexao = new Conexao();
+        $tarefaService = new TarefaService($conexao,$tarefa);
+        $tarefas = $tarefaService->recuperar();
+    }else if ($acao == 'atualizar'){
+        $tarefa = new Tarefa();
+        $tarefa->__set('id',$_POST['id']);
+        $tarefa->__set('tarefa',$_POST['tarefa']);
+        $conexao = new Conexao();
+        $tarefaService = new TarefaService($conexao,$tarefa);
+        if (isset($_GET['pag'])&&$_GET['pag']=='index'){
+            header('Location: index.php');
+        }
+        if($tarefaService->atualizar() && $_GET['pag']!='index'){
+            header('Location: todas_tarefas.php');
+        }
+    }else if ($acao == 'remover'){
+        $tarefa = new Tarefa();
+        $tarefa->__set('id', $_GET['id']);
+        $conexao = new Conexao();
+        $tarefaService = new TarefaService($conexao,$tarefa);
+        $tarefaService->remover();
+        if (isset($_GET['pag'])&&$_GET['pag']=='index'){
+            header('Location: index.php');
+        }
+        if ($_GET['pag']!='index') header('Location: todas_tarefas.php');
+    }else if ($acao == 'marcarRealizada'){
+        $tarefa = new Tarefa();
+        $tarefa->__set('id', $_GET['id']);
+        $tarefa->__set('id_status',2);
+        $conexao = new Conexao();
+        $tarefaService = new TarefaService($conexao,$tarefa);
+        $tarefaService->marcarRealizada();
+        if (isset($_GET['pag'])&&$_GET['pag']=='index'){
+            header('Location: index.php');
+        }
+        if ($_GET['pag']!='index') header('Location: todas_tarefas.php');
+    }else if ($acao == 'recuperarTarefasPendentes'){
+        $tarefa = new Tarefa();
+        $conexao = new Conexao();
+        $tarefaService = new TarefaService($conexao,$tarefa);
+        $tarefas = $tarefaService->recuperar();
+        //header('Location: index.php');
+    }
+
+
+?>
